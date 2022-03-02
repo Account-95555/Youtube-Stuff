63b97b91aaeae0de19ba67a9a1895eaaed3020d6
Account-95555 <100207649+Account-95555@users.noreply.github.com>
2022-03-02 18:27:14 +0800

    Update lister.py

--- lister.py
+++ lister.py
@@ -1,5 +1,7 @@
-#Keep in mind that this program only runs once, you have to restart it in order
-#to run it again!
+#This program outputs a github list format based on what number you input. This was created to make the music listing easier
+
+#Keep in mind that this program only runs once, you have to restart it in order to run it again!
+
 initial = 1
 numbers = int(input("Enter how many numbers your list needs (outputted as (no.).): "))
 while initial < numbers + 1:
